f453539b63f2e9c1d72ec5e391da14cf24779778
Amy DiPasquale <amyd@amys-mbp.lan>
2019-01-25 13:17:22 -0500

    TA-1532 mocha tests to mock axios call

--- src/service/events.test.js
+++ src/service/events.test.js
@@ -1,19 +1,63 @@
-/**
- * @jest-environment node
-*/
+/* eslint-env mocha */
+
+let axios = require('axios')
+let sinon = require('sinon')
+let chai = require('chai')
+chai.should()
 
 var events = require('./events')
-const PERSONAL_OAUTH_TOKEN = '54RORZVKSOARZFYDIQ37'
-const ORGANIZATION_ID = '217145347272'
 
 describe('Events', () => {
+  let axiosGetStub
+
+  before(() => {
+    axiosGetStub = sinon.stub(axios, 'get')
+  })
+
+  afterEach(() => {
+    axiosGetStub.reset()
+  })
+
   it('gets an organization ID', async () => {
+    const PERSONAL_OAUTH_TOKEN = 'MOCK_TOKEN_ID'
+    const expectedOrganizationId = '100'
+    const mockResponseOrganizationId = {
+      data: {
+        organizations: [
+          { _type: 'organization',
+            name: 'SBA GOV',
+            vertical: 'default',
+            locale: null,
+            image_id: null,
+            id: expectedOrganizationId
+          }
+        ]
+      }
+    }
+
+    axiosGetStub.returns(mockResponseOrganizationId)
     const organizationId = await events.getOrganizationId(PERSONAL_OAUTH_TOKEN)
-    expect(organizationId).toBe(ORGANIZATION_ID)
+    organizationId.should.eql(expectedOrganizationId)
   })
 
   it('gets a list of events for an organization', async () => {
-    const eventBriteEvents = await events.fetchEvents(PERSONAL_OAUTH_TOKEN, ORGANIZATION_ID)
+    const PERSONAL_OAUTH_TOKEN = 'MOCK_TOKEN_ID'
+    const expectedOrganizationId = '100'
+    const expectedEvents = [
+      { id: '111' },
+      { id: '222' }
+    ]
+    const mockResponseEvents = {
+      data: {
+        events: [
+          { id: '111' },
+          { id: '222' }
+        ]
+      }
+    }
 
+    axiosGetStub.returns(mockResponseEvents)
+    const eventBriteEvents = await events.fetchEvents(PERSONAL_OAUTH_TOKEN, expectedOrganizationId)
+    eventBriteEvents.should.eql(expectedEvents)
   })
 })
