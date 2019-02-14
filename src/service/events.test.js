fb404172606a165cd25c6e5554bb98a49552035d
Avery Smith <avery@averysmith.pro>
2019-02-14 15:51:45 -0500

    Feature ta 1684 (#13)
    * TA-1684 added zipcode filtering
    
    * TA-1684 added test
    
    * TA-1684 updated test description
    
    * TA-1684 updated zipcode reference to address

--- src/service/events.test.js
+++ src/service/events.test.js
@@ -71,4 +71,13 @@ describe('Eventbrite client', () => {
     const result = await events.fetchEvents()
     result.should.eql(expected)
   })
+
+  it('filter event by zipcode', async () => {
+    const params = {
+      address: '20000'
+    }
+    const expected = eventsData.filter( item => item.location.zipcode === params.address)
+    const result = await events.fetchEvents(params)
+    expected.length.should.eql(result.length)
+  })
 })
