058ddb50722be5d6be5ec988e002bc945c37a492
Aaron Brooks <brooksa321@gmail.com>
2019-02-14 00:18:10 -0500

    TA-1567 Added date range filetering logic to event results

--- src/service/events.js
+++ src/service/events.js
@@ -1,3 +1,4 @@
+const moment = require('moment')
 const axios = require('axios')
 const config = require('../config')
 const mockDataEvents = require('./mock-events-data.json')
@@ -23,13 +24,38 @@ async function getOrganizationId () {
 function fetchEvents (query) {
   let result = mockDataEvents
   const queryObj = query || {}
-  const { address } = queryObj
+  const { address, dateRange } = queryObj
   // if zipcode param is available
     // filter by zipcode
   if (address) {
     result = result.filter( item => {
       return item.location.zipcode === address
     })
+  }
+
+  if (dateRange) {
+    let now = moment().format()
+
+    switch (dateRange) {
+      case 'today':
+        let today = moment(now).format("YYYY-MM-DD")
+        result = result.filter(result => today == moment(result.start_date).format("YYYY-MM-DD"))
+        break;
+      case 'tomorrow':
+        let tomorrow = moment(moment(now).add(1, 'd')).format("YYYY-MM-DD")
+        result = result.filter(result => tomorrow == moment(result.start_date).format("YYYY-MM-DD")) 
+        break;
+      case '7days':
+        let sevenDay = moment(now).add(7, 'd')
+        result = result.filter(result => moment(result.start_date).isBetween(now, sevenDay))
+        break;
+      case '30days':
+        let thirtyDay = moment(now).add(30, 'd')
+        result = result.filter(result => moment(result.start_date).isBetween(now, thirtyDay))
+        break;
+      default:
+    }
+    
   }
 
   return result
