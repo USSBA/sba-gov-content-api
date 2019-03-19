d9d4a7e328071673605f08828e75b54f0cc2b1aa
Kevin Lee <ki.lee@sba.gov>
2019-03-19 12:41:56 -0400

    T1-921 Undo change to config.js

--- src/config.js
+++ src/config.js
@@ -2,13 +2,9 @@ module.exports = {
   cloudSearch: {
     searchEndpoint: process.env.CLOUDSEARCH_SEARCH_ENDPOINT,
     officeEndpoint: process.env.CLOUDSEARCH_OFFICE_ENDPOINT
-    // searchEndpoint: 'doc-mint-hdgvyg66mwvfnu46tzaszesh5u.us-east-1.cloudsearch.amazonaws.com',
-    // officeEndpoint: 'doc-mint-offices-mjhazqnua7k5nqkxojddfhkrw4.us-east-1.cloudsearch.amazonaws.com'
   },
   contentBucket: process.env.CONTENT_BUCKET,
   zipCodeDynamoDbTable: process.env.ZIP_CODE_DYNAMODB_TABLE,
-  // contentBucket: 'kevin-content-sba-gov',
-  // zipCodeDynamoDbTable: 'ZipCodesLower',
   eventsApi: {
     hostname: process.env.EVENTS_HOSTNAME,
     endpoint: process.env.EVENTS_API_ENDPOINT,
