e46ab7397ce4f5a1a11a0d2254cba7c816d8dabe
Avery Smith <avery@averysmith.pro>
2019-05-03 13:55:06 -0400

    TA-2279 linted files

--- src/service/authors.test.js
+++ src/service/authors.test.js
@@ -6,18 +6,18 @@ const { getAuthors } = require('./authors.js')
 const authors = require('./authors.json')
 
 describe('authors', () => {
-	it('should return json data', () => {
-		const result = getAuthors()
+  it('should return json data', () => {
+    const result = getAuthors()
     	const expected = authors
     	expect(result).to.deep.equal(expected)
-	})
-	it('should contain an array', () => {
-		const result = getAuthors()
-		expect(Array.isArray(result)).to.be.true
-	})
-	it('array should contain only numbers', () => {
-		const expected = 'number'
-		const result = getAuthors()
-		authors.forEach( author => expect(typeof author).to.equal(expected))
-	})
+  })
+  it('should contain an array', () => {
+    const result = getAuthors()
+    expect(Array.isArray(result)).to.be.true
+  })
+  it('array should contain only numbers', () => {
+    const expected = 'number'
+    const result = getAuthors()
+    authors.forEach(author => expect(typeof author).to.equal(expected))
+  })
 })
