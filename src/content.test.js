0fafa6866f6f0ce6698bdbba9153882f460f0d59
Amy DiPasquale <ald5044@gmail.com>
2019-06-20 14:22:58 -0400

    TA-2651 add tests for content.js file functions

--- src/content.test.js
+++ src/content.test.js
@@ -8,36 +8,109 @@ let expect = chai.expect
 let contentHandler
 let events = require('./service/events.js')
 const HttpStatus = require('http-status-codes')
+const config = require('./config.js')
 
 describe('# Content Handler', () => {
-  let eventStub
+  let eventStub, getBackendSourceToggleStub
 
   before(() => {
     eventStub = sinon.stub(events, 'fetchEventById')
+    getBackendSourceToggleStub = sinon.stub(config.eventsApi, 'getBackendSourceToggle')
     contentHandler = require('./content.js') // this is a small workaround for the way the content handler imports service modules
   })
 
   afterEach(() => {
     eventStub.reset()
+    getBackendSourceToggleStub.reset()
   })
 
   after(() => {
     eventStub.restore()
+    getBackendSourceToggleStub.restore()
   })
 
-  describe('fetchContentById', () => {
+  // TODO: remove this describe block when feature flag, getBackendSourceToggle, for events backend is removed
+  // fetchEventById will no longer be a function
+  describe('fetchContentById for event id mapping', () => {
     it('should respond with event data when a valid id is given in the path', async () => {
       let data = { title: 'thisisatitle' }
+
+      getBackendSourceToggleStub.returns(false)
       eventStub.withArgs(1).returns(data)
+
+      let expected = { statusCode: HttpStatus.OK, body: data }
       let result = await contentHandler.fetchContentById({ type: 'event', id: 1 })
-      let expected = {statusCode: HttpStatus.OK, body: data}
+
       expect(result).to.deep.equal(expected)
     })
 
     it('should respond with a 404 when an invalid id is given in the path', async () => {
+      getBackendSourceToggleStub.returns(false)
       eventStub.returns(null)
+
+      let expected = { statusCode: HttpStatus.NOT_FOUND, body: 'Unable to find event with id 1' }
       let result = await contentHandler.fetchContentById({ type: 'event', id: 1 })
-      let expected = {statusCode: HttpStatus.NOT_FOUND, body: 'Unable to find event with id 1'}
+
+      expect(result).to.deep.equal(expected)
+    })
+
+    it('should display invalid endpoint message for event data with getBackendSourceToggle set to true', async () => {
+      getBackendSourceToggleStub.returns(true)
+
+      let expected = { statusCode: HttpStatus.NOT_FOUND, body: 'Unknown type event' }
+      let result = await contentHandler.fetchContentById({ type: 'event', id: 1 })
+
+      expect(result).to.deep.equal(expected)
+    })
+  })
+
+  describe('fetchContentById', () => {
+    beforeEach(() => {
+      getBackendSourceToggleStub.returns(true)
+    })
+
+    it('should display invalid endpoint message for unknown data type', async () => {
+      const dataType = 'fake data type'
+
+      let expected = { statusCode: HttpStatus.NOT_FOUND, body: `Unknown type ${dataType}` }
+      let result = await contentHandler.fetchContentById({ type: dataType, id: 1 })
+
+      expect(result).to.deep.equal(expected)
+    })
+
+    it('should display invalid endpoint message when no type and/or id is declared', async () => {
+      let expected = { statusCode: HttpStatus.BAD_REQUEST, body: `Incorrect request format: missing type or id` }
+      let result = await contentHandler.fetchContentById()
+
+      expect(result).to.deep.equal(expected)
+    })
+  })
+
+  describe('fetchContentByType', () => {
+    beforeEach(() => {
+      getBackendSourceToggleStub.returns(true)
+    })
+
+    it('should respond with status code OK when valid data type is given in the path', async () => {
+      let expected = { statusCode: HttpStatus.OK }
+      let result = await contentHandler.fetchContentByType({ type: 'nodes' })
+
+      expect(result.statusCode).to.deep.equal(expected.statusCode)
+    })
+
+    it('should display invalid endpoint message for unknown data type', async () => {
+      const dataType = 'fake data type'
+
+      let expected = { statusCode: HttpStatus.NOT_FOUND, body: `Unknown type ${dataType}` }
+      let result = await contentHandler.fetchContentByType({ type: dataType })
+
+      expect(result).to.deep.equal(expected)
+    })
+
+    it('should display invalid endpoint message when no type is declared', async () => {
+      let expected = { statusCode: HttpStatus.BAD_REQUEST, body: `Incorrect request format: missing type` }
+      let result = await contentHandler.fetchContentByType()
+
       expect(result).to.deep.equal(expected)
     })
   })
