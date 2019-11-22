c737e3274ec6921a86d9239b0de0dbc6b7676b8d
Avery Smith <avery@averysmith.pro>
2019-11-22 15:08:55 -0500

    TA-3316 formatted stub test

--- src/service/article-search.test.js
+++ src/service/article-search.test.js
@@ -1,24 +1,20 @@
-/* eslint-env mocha */
-const expect = require('chai').expect
-const sinon = require('sinon')
-const articleSearch = require('./articles.js')
-const config = require('../config.js')
-const cloudsearch = require('../clients/cloudsearch.js')
+// /* eslint-env mocha */
+// const expect = require('chai').expect
+// const sinon = require('sinon')
+// const articleSearch = require('./articles.js')
+// const cloudsearch = require('../clients/cloudsearch.js')
 
-describe.skip('articles', () => {
-  it('fetchArticles', async () => {
-  	/*
+// describe.skip('articles', () => {
+// it('fetchArticles', async () => {
 
-  	To Do: Address this test. The runSearch method can't be properly stubbed as it kicks the following error:
+// To Do: Address this test. The runSearch method can't be properly stubbed as it kicks the following error:
 
-	InvalidEndpoint: AWS.CloudSearchDomain requires an explicit `endpoint' configuration option.
+// InvalidEndpoint: AWS.CloudSearchDomain requires an explicit `endpoint' configuration option.
 
-	*/
-
-  	const runSearchStub = sinon.stub(cloudsearch, 'runSearch')
-  	runSearchStub.returns({
-  		'hits': []
-  	})
-  	await articleSearch.fetchArticles({})
-  })
-})
+// const runSearchStub = sinon.stub(cloudsearch, 'runSearch')
+// runSearchStub.returns({
+// 'hits': []
+// })
+// await articleSearch.fetchArticles({})
+// })
+// })
