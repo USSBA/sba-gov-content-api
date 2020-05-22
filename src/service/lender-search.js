53bea8f059ffcdedfc36cb2aaf5ac37cb4c82e7a
Parissa <parissa.eggleston@gmail.com>
2020-05-22 17:14:03 -0400

    fix linter messages

--- src/service/lender-search.js
+++ src/service/lender-search.js
@@ -58,9 +58,9 @@ function buildParams (query, geo) {
 function titleCase (str) {
   var splitStr = str.toLowerCase().split(' ')
   for (var i = 0; i < splitStr.length; i++) {
-    splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1)   
+    splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1)
   }
-  return splitStr.join(' '); 
+  return splitStr.join(' ')
 }
 
 function dedupSuggester (result) {
