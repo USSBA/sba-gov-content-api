f49dc6eb18129099763587a782fb49bb18e99fc2
Parissa <parissa.eggleston@gmail.com>
2020-05-27 12:09:58 -0400

    remove var use let

--- src/service/lender-search.js
+++ src/service/lender-search.js
@@ -57,7 +57,7 @@ function buildParams (query, geo) {
 
 function titleCase (str) {
   var splitStr = str.toLowerCase().split(' ')
-  for (var i = 0; i < splitStr.length; i++) {
+  for (let i = 0; i < splitStr.length; i++) {
     splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1)
   }
   return splitStr.join(' ')
