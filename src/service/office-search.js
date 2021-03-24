a1cea6fac686845685afdca808d74c95d2ed3d9c
Ksenia Coulter <kcoulter@gmail.com>
2021-03-24 09:53:19 -0400

    Reworking the office type filter

--- src/service/office-search.js
+++ src/service/office-search.js
@@ -27,10 +27,10 @@ function buildQuery (query) {
 
 function buildFilters (service, type) {
   const officeTypes = type ? type.split(',') : []
-  const formattedTypes = officeTypes.map((type) => { return cloudsearch.formatString(type) })
+  const formattedTypes = officeTypes.map((type) => { return `office_type:${cloudsearch.formatString(type)}` })
   let filters = [
     service ? `office_service: '${cloudsearch.formatString(service)}'` : null,
-    formattedTypes.length > 0 ? `office_type: (or ${formattedTypes.join(' ')})` : null
+    formattedTypes.length > 0 ? `(or ${formattedTypes.join(' ')})` : null
   ]
   filters = filters.filter(item => item)
   let filterString = null
